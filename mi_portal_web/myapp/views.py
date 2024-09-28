7435c09b648210115f5e11cf75fc72c3f1e4c072
ciglesiasvera <ciglesiasvera@gmail.com>
2024-09-28 02:47:10 -0300

    Añadiendo contenido dinamico

--- mi_portal_web/myapp/views.py
+++ mi_portal_web/myapp/views.py
@@ -19,10 +19,20 @@ def blog(request):
     return render(request, 'myapp/blog.html')
 
 def portfolio(request):
-    return render(request, 'myapp/portfolio.html')
+    projects = [
+        {'name':" Pagina de Restaurante", 'description': 'Proyecto para cadena de restaurantes chilenos', 'image':'https://media.vandal.net/i/1280x720/10-2023/17/202310171642263_4.jpg'},
+        {'name':" Pagina de Inmobiliaria", 'description': 'Proyecto para venta de departamentos', 'image':'https://image.api.playstation.com/vulcan/ap/rnd/202211/2222/l8QTN7ThQK3lRBHhB3nX1s7h.png'},
+        {'name':" Pagina de Escuela", 'description': 'Proyecto para Escuela de Psicologia', 'image':'https://cdn.sortiraparis.com/images/80/66131/1024452-star-wars-bientot-un-jeu-the-mandalorian-developpe-par-respawn-entertainment.jpg'},
+    ]
+    return render(request, 'myapp/portfolio.html', {'projects': projects})
 
 def teams(request):
     return render(request, 'myapp/teams.html')
 
 def testimonial(request):
-    return render(request, 'myapp/testimonial.html')
+    testimonial_data = [
+        {'nombre':" Roberto", 'puesto':" CEO of MIT", 'testimonio':" Excelente servicio"},
+        {'nombre':" Andrea", 'puesto':" CEO of MIT", 'testimonio':" Excelente servicio"},
+        {'nombre':" Carlos", 'puesto':" CEO of MIT", 'testimonio':" Excelente servicio"},
+    ]
+    return render(request, 'myapp/testimonial.html', {'testimonial': testimonial_data})
